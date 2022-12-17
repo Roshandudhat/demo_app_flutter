af488a5b3b6818dc765209ebdac2606bd3c7c033
Roshan <roshandudhat10@gmail.com>
2022-12-17 15:59:28 +0530

    Demo flutter app2

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page Github push'),
+      home: const MyHomePage(titlePush: 'Flutter Demo Home Page Github push'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.titlePush}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -41,7 +41,10 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+
+
+
+  final String titlePush;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -73,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.titlePush),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
